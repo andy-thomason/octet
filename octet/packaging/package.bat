9cea13934d3a5e3cdce7ece87977364f2fcbccb6
andy_thomason <andy@andythomason.com>
2013-07-31 10:03:14 +0100

    Added documentation Fixed package batch file.

--- octet/packaging/package.bat
+++ octet/packaging/package.bat
@@ -1,2 +1,3 @@
-zip -r -X install\octet-1.0.zip src\*.* vc2010\framework.vcxproj vc2010\framework.vcxproj.filters assets\*.wav assets\*.* xcode\*.* lib\*.lib -x *.au 
-copy install\octet-1.0.zip c:\Users\andyt\andythomason.com\htdocs\files 
+del install\octet-%1.zip /q
+zip -r -X install\octet-%1.zip src\*.* vc2010\framework.sln vc2010\framework.vcxproj vc2010\framework.vcxproj.filters vc2010\animation\animation.vcxproj vc2010\animation\animation.vcxproj.filters assets\*.* assets\invaderers\*.* doc\*.* xcode\*.* lib\*.lib -x *.au -x *.xcf
+copy install\octet-%1.zip c:\Users\andyt\andythomason.com\htdocs\files 
