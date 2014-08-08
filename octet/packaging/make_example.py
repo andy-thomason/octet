399da3a1aaf38f764fa138c62401d2eedb276d87
unknown <andyt@andyt-PCx64.domain.snsys.com>
2014-08-08 14:21:28 +0100

    add vc2013 projects

--- octet/packaging/make_example.py
+++ octet/packaging/make_example.py
@@ -3,24 +3,31 @@ import os
 import dircache
 import string
 
-proto_dir = 'make_example_prototype/'
-target_dir = '../src/examples/'
+proto_dir = 'packaging/make_example_prototype/'
+target_dir = 'src/examples/'
 
 def make_example(projname):
-  os.mkdir(target_dir + projname)
+  try:
+    os.mkdir(target_dir + projname)
+  except:
+    pass
   for fname in dircache.listdir(proto_dir):
     dest_name = string.replace(fname, 'prototype', projname)
-    out = open(target_dir + projname + '/' + dest_name, "wb")
-    for line in open(proto_dir + fname):
-      line = string.replace(line, 'prototype', projname)
-      out.write(line)
+    try:
+      test = open(target_dir + projname + '/' + dest_name, "rb")
+      test.close()
+    except:
+      out = open(target_dir + projname + '/' + dest_name, "wb")
+      for line in open(proto_dir + fname):
+        line = string.replace(line, 'prototype', projname)
+        out.write(line)
 
 
 num_args = len(sys.argv)
 
 if num_args == 1 or "--help" in sys.argv:
-  print("\nmakes a new octet project in src/examples")
-  print("\nusage: make_example.py projectname")
+  print("\nmakes or updates a new octet project in src/examples")
+  print("\nusage: packaging\make_example.py projectname")
   exit()
 
 for i in range(1,num_args):
