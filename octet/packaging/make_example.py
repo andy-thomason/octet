2b40a3f4a2721dff93b281fec70e86a19ee6a167
Andy Thomason <andythomason@Andys-iMac.local>
2014-10-13 09:25:45 +0100

    clean up uncomitted changes

--- octet/packaging/make_example.py
+++ octet/packaging/make_example.py
@@ -2,6 +2,7 @@ import sys
 import os
 import dircache
 import string
+import stat
 
 proto_dir = 'src/examples/example_prototype/'
 target_dir = 'src/examples/'
@@ -9,25 +10,26 @@ target_dir = 'src/examples/'
 def make_example(src_dir, dest_dir, projname):
   print("make", src_dir, dest_dir)
   try:
-    os.mkdir(target_dir + projname)
+    os.mkdir(dest_dir)
   except:
     pass
   for fname in dircache.listdir(src_dir):
+    mode = os.stat(src_dir + fname).st_mode
     if 'prototype' in fname:
       dest_name = string.replace(fname, 'prototype', projname)
     else:
       dest_name = fname
     print("fname", fname)
-    if '.xcodeproj' in dest_name:
-      make_example(src_dir + fname, dest_dir + dest_name, projname)
+    if stat.S_ISDIR(mode):
+      make_example(src_dir + fname + '/', dest_dir + '/' + dest_name + '/', projname)
     else:
-      print(target_dir + projname + '/' + dest_name)
+      print(dest_dir + '/' + dest_name)
       try:
-        test = open(dest_dir + projname + '/' + dest_name, "rb")
+        test = open(dest_dir + '/' + dest_name, "rb")
         test.close()
       except:
-        out = open(dest_dir + projname + '/' + dest_name, "wb")
-        for line in open(proto_dir + fname):
+        out = open(dest_dir + '/' + dest_name, "wb")
+        for line in open(src_dir + fname):
           line = string.replace(line, 'prototype', projname)
           out.write(line)
 
@@ -42,7 +44,7 @@ if num_args == 1 or "--help" in sys.argv:
 for i in range(1,num_args):
   arg = sys.argv[i];
   if arg[0] != '-':
-    make_example(proto_dir, target_dir, arg)
+    make_example(proto_dir, target_dir + arg, arg)
   else:
     print("unrecognised option " + arg)
     exit()
