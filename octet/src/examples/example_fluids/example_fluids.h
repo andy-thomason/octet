0ce1dea047732dbddab8d9ad56bbc7bc1a201634
Andy Thomason <andy@andythomason.com>
2015-01-26 13:43:27 +0000

    updated fluids

--- octet/src/examples/example_fluids/example_fluids.h
+++ octet/src/examples/example_fluids/example_fluids.h
@@ -5,6 +5,11 @@
 // Modular Framework for OpenGLES2 rendering on multiple platforms.
 //
 // fluid example based on Joss Stam paper.
+//
+
+#include <valarray>
+#include <memory>
+
 namespace octet {
   /// Scene containing a box with octet.
   class example_fluids : public app {
@@ -61,11 +66,8 @@ namespace octet {
         add_attribute(attribute_color, 3, GL_FLOAT, 12);
       }
 
-      void add_source( int N, float * x, float * s, float dt ) {
-	      int i, size=(N+2)*(N+2);
-	      for ( i=0 ; i<size ; i++ ) x[i] += dt*s[i];
-      }
-
+      /// clamp edges to same (or negative) value of inner pixels
+      /// to act as a barrier
       void set_boundary( int N, int b, float * x ) {
         auto IX = [=](int i, int j) { return i +(N+2)*j; };
 
@@ -82,51 +84,68 @@ namespace octet {
 	      x[IX(N+1,N+1)] = 0.5f*(x[IX(N,N+1)]+x[IX(N+1,N)]);
       }
 
+      /// solve diffusion equations (propagation into adjacent cells)
+      /// by repeated aplication of a weighted average.
+      /// Use a fixed number of iterations. 
+      /// at the end x should not change (you should test this yourself)
       void gauss_siedel( int N, int b, float * x, float * x0, float a, float c ) {
         auto IX = [=](int i, int j) { return i +(N+2)*j; };
 
 	      for ( int k=0 ; k<20 ; k++ ) {
 		      for ( int i=1 ; i<=N ; i++ ) {  
             for ( int j=1 ; j<=N ; j++ ) {
-			        x[IX(i,j)] =(x0[IX(i,j)] + a*(x[IX(i-1,j)]+x[IX(i+1,j)]+x[IX(i,j-1)]+x[IX(i,j+1)]))/c;
+			        x[IX(i,j)] = (x0[IX(i,j)] + a*(x[IX(i-1,j)]+x[IX(i+1,j)]+x[IX(i,j-1)]+x[IX(i,j+1)]))/c;
             }
           }
 		      set_boundary( N, b, x );
 	      }
       }
 
+      /// calculate diffision by approximating with a weighted average
       void diffusion( int N, int b, float * x, float * x0, float diff, float dt ) {
-	      float a=dt*diff*N*N;
+	      float a = dt * diff * (N * N);
 	      gauss_siedel( N, b, x, x0, a, 1+4*a );
       }
 
+      /// carry a quantity (velocity or density) from one cell to another.
       void advection_step( int N, int b, float * d, float * d0, float * u, float * v, float dt ) {
         auto IX = [=](int i, int j) { return i +(N+2)*j; };
 
 	      float dt0 = dt*N;
 		    for ( int i=1 ; i<=N ; i++ ) {  
           for ( int j=1 ; j<=N ; j++ ) {
+            // (x, y) is the address to copy from
 		        float x = i-dt0*u[IX(i,j)], y = j-dt0*v[IX(i,j)];
 
-		        if (x<0.5f) x=0.5f;
-            if (x>N+0.5f) x=N+0.5f;
+            // clamp x and y
+		        if (x<0.5f) x=0.5f; else if (x>N+0.5f) x=N+0.5f;
+		        if (y<0.5f) y=0.5f; else if (y>N+0.5f) y=N+0.5f;
+
+            // s1 and s0 are lerp coordinates [0,1) within the source cell
             int i0=(int)x, i1=i0+1;
-
-		        if (y<0.5f) y=0.5f;
-            if (y>N+0.5f) y=N+0.5f;
             int j0=(int)y, j1=j0+1;
+		        float s1 = x - i0, s0 = 1 - s1;
+            float t1 = y - j0, t0 = 1 - t1;
 
-		        float s1 = x-i0, s0 = 1-s1, t1 = y-j0, t0 = 1-t1;
-
-		        d[IX(i,j)] = s0*(t0*d0[IX(i0,j0)]+t1*d0[IX(i0,j1)])+s1*(t0*d0[IX(i1,j0)]+t1*d0[IX(i1,j1)]);
+            // sample the source
+		        d[IX(i,j)] =
+              s0*(t0*d0[IX(i0,j0)]+t1*d0[IX(i0,j1)]) +
+              s1*(t0*d0[IX(i1,j0)]+t1*d0[IX(i1,j1)])
+            ;
           }
         }
+
+        // copy values out to the boundary.
 	      set_boundary( N, b, d );
       }
 
+      // stablisation step. adjust the velocity to prevent increase in energy
+      // in the system.
       void project( int N, float * u, float * v, float * p, float * div ) {
         auto IX = [=](int i, int j) { return i +(N+2)*j; };
 
+        // calculate divergence into div
+        // set initial value of p
 		    for ( int i=1 ; i<=N ; i++ ) {  
           for ( int j=1 ; j<=N ; j++ ) {
 		        div[IX(i,j)] = -0.5f*(u[IX(i+1,j)]-u[IX(i-1,j)]+v[IX(i,j+1)]-v[IX(i,j-1)])/N;
@@ -134,48 +153,59 @@ namespace octet {
           }
         }
 
+        // copy pixels to boundary
 	      set_boundary( N, 0, div );
         set_boundary( N, 0, p );
 
+        // p += div[x+/-1, y+/-1] * 4;
 	      gauss_siedel( N, 0, p, div, 1, 4 );
 
+        // calculate velocity from pressure-like "p"
 		    for ( int i=1 ; i<=N ; i++ ) {  
           for ( int j=1 ; j<=N ; j++ ) {
+            // u from left and right
 		        u[IX(i,j)] -= 0.5f*N*(p[IX(i+1,j)]-p[IX(i-1,j)]);
+
+            // v from up and down.
 		        v[IX(i,j)] -= 0.5f*N*(p[IX(i,j+1)]-p[IX(i,j-1)]);
           }
         }
 
+        // copy velocity to boundary
 	      set_boundary( N, 1, u );
         set_boundary( N, 2, v );
       }
 
+      /// Given a velocity field, carry a value around the simulation
+      /// and diffuse the value.
       void density_step( int N, float * x, float * x0, float * u, float * v, float diff, float dt ) {
-	      add_source( N, x, x0, dt );
-
+        // apply diffusion to density. If there is no velocity, the value will still spread.
 	      std::swap( x0, x );
         diffusion( N, 0, x, x0, diff, dt );
 
+        // use the velocity field to carry density around.
 	      std::swap( x0, x );
         advection_step( N, 0, x, x0, u, v, dt );
       }
 
+      /// Compute the new velocity field.
       void velocity_step( int N, float * u, float * v, float * u0, float * v0, float visc, float dt ) {
-	      add_source( N, u, u0, dt );
-        add_source( N, v, v0, dt );
-
+        // diffuse into neighouring cells
 	      std::swap( u0, u );
         diffusion( N, 1, u, u0, visc, dt );
 	      std::swap( v0, v );
         diffusion( N, 2, v, v0, visc, dt );
 
+        // stabilise the system using poisson
 	      project( N, u, v, u0, v0 );
 	      std::swap( u0, u );
         std::swap( v0, v );
 
+        // use advection to move the velocity itself
 	      advection_step( N, 1, u, u0, u0, v0, dt );
         advection_step( N, 2, v, v0, u0, v0, dt );
 
+        // stabilise the system using poisson
 	      project( N, u, v, u0, v0 );
       }
 
@@ -190,19 +220,25 @@ namespace octet {
 
         //printf("dtot=%f\n", std::accumulate(density.cbegin(), density.cend(), 0.0f));
 
+        // fill the input values
         std::fill(prev_vx.begin(), prev_vx.end(), 0.0f);
         std::fill(prev_vy.begin(), prev_vy.end(), 0.0f);
         std::fill(prev_density.begin(), prev_density.end(), 0.0f);
 
+        // you could use a UI to do this.
         float c = math::cos(frame_number*0.01f);
         float s = math::sin(frame_number*0.01f);
-        prev_density[50 +(dim.x()+1) * 50] += 100;
-        u_prev[50 +(dim.x()+1) * 50] += c * 100;
-        v_prev[50 +(dim.x()+1) * 50] += s * 100;
+        density[50 +(dim.x()+1) * 50] += 100 * dt;
+        u[50 +(dim.x()+1) * 50] += c * (100 * dt);
+        v[50 +(dim.x()+1) * 50] += s * (100 * dt);
 
+        // step the simulation.
 	      //get_from_UI( dens_prev, u_prev, v_prev );
+        long long t0 = __rdtsc();
 	      velocity_step( N, u, v, u_prev, v_prev, visc, dt );
 	      density_step( N, dens, dens_prev, u, v, diff, dt );
+        long long t1 = __rdtsc();
+        printf("%lld clocks\n", t1-t0);
 
         //printf("dtot=%f\n", std::accumulate(density.cbegin(), density.cend(), 0.0f));
 
@@ -249,7 +285,7 @@ namespace octet {
     void draw_world(int x, int y, int w, int h) {
       int vx = 0, vy = 0;
       get_viewport_size(vx, vy);
-      app_scene->begin_render(vx, vy);
+      app_scene->begin_render(vx, vy, vec4(0, 0, 0, 1));
 
       the_mesh->update(get_frame_number());
 
